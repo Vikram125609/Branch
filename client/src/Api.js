7d6b7e104eaf8c4ea2e4c3a84a58be16f99137b9
Vikram Singh <vs125609@gmail.com>
2023-08-18 09:42:45 +0530

    changed the ip

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-export const registerUser = (data) => axios.post('http://13.233.107.85:8080/register', data);
-export const totalPendingQuery = () => axios.get('http://13.233.107.85:8080/totalPendingQuery');
-export const replyToClient = (data) => axios.post('http://13.233.107.85:8080/replyToClient',data);
-export const fetchPreviousQuestion = (data) => axios.post('http://13.233.107.85:8080/fetchPreviousQuestion', data);
-export const fetchPendingQuestion = (data) => axios.post('http://13.233.107.85:8080/fetchPendingQuestion', data);
-export const replyToPendingQuestion = (data) => axios.post('http://13.233.107.85:8080/replyToPendingQuestion',data);
+export const registerUser = (data) => axios.post('http://3.111.218.103:8080/register', data);
+export const totalPendingQuery = () => axios.get('http://3.111.218.103:8080/totalPendingQuery');
+export const replyToClient = (data) => axios.post('http://3.111.218.103:8080/replyToClient',data);
+export const fetchPreviousQuestion = (data) => axios.post('http://3.111.218.103:8080/fetchPreviousQuestion', data);
+export const fetchPendingQuestion = (data) => axios.post('http://3.111.218.103:8080/fetchPendingQuestion', data);
+export const replyToPendingQuestion = (data) => axios.post('http://3.111.218.103:8080/replyToPendingQuestion',data);
